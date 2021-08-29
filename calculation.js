b9c616a478fff38882fdca4c4d1f9b5bd89c098b
oosakiken1 <62978966+oosakiken1@users.noreply.github.com>
2021-08-29 17:29:57 +0900

    change

--- calculation.js
+++ calculation.js
@@ -6,7 +6,6 @@ const CHK_LIMIT = document.getElementById("limit");
 const CHK_DETAIL = document.getElementById("detail");
 const CHK_PERIOD = document.getElementById("period");
 
-
 BTN_EXEC.onclick = function () {
     const text = TEXTAREA.value.replace(/\r\n|\r/g, '\n');
     var resultText = '';
@@ -14,30 +13,31 @@ BTN_EXEC.onclick = function () {
     const lines = text.split('\n');
 
     for (const line of lines) {
-        var kutenReg = null;
+        var sentenceReg = null;
         if  (CHK_PERIOD.checked) {
-            kutenReg = /[^。．]*[。．]*/g;
+            sentenceReg = /[^。．]*[。．]*/g;
         }  else {
-            kutenReg = /[^。]*。*/g;
+            sentenceReg = /[^。]*。*/g;
         }
 
-        // const sentences = line.match(/[^。]*。*/g);
-        const sentences = line.match(kutenReg);
+        const sentences = line.match(sentenceReg);
 
         for (const sentence of sentences) {
             if (sentence === '') { continue }
             const sentenceLength = sentence.length;
-            const kanjiOver5 = countKanjiOver5(sentence);
-            const kanaOver9 = countKanaOver9(sentence);
-            const wIndex = 0.0030933 + sentenceLength * 0.0028964 + kanjiOver5 * 0.0753041 + kanaOver9 * 0.2166971;
+            const kanji6 = countKanji6(sentence);
+            const kana10 = countKana10(sentence);
+            const wakarinikusaIndex = 0.0030933 + sentenceLength * 0.0028964 + kanji6 * 0.0753041 + kana10 * 0.2166971;
 
-            if (CHK_DETAIL.checked) {
-                const wIndexText = `<span style="font-size:50%">(${sentenceLength}-${kanjiOver5}-${kanaOver9}=${Math.round(wIndex * 100) / 100})</span>`;
-                resultText = resultText + wIndexText;
-            }
-            if (wIndex >= 0.30 || CHK_LIMIT.checked) {
-                const wIndexColor = Math.round(256 * (1 - wIndex));
-                resultText = resultText + `<span style="background-color:rgb(255,${wIndexColor},${wIndexColor});">${sentence}</span>`;
+            if (wakarinikusaIndex >= 0.30 || CHK_LIMIT.checked) {
+ 
+                if (CHK_DETAIL.checked) {
+                    const detailText = `<span style="font-size:50%">(${sentenceLength}-${kanji6}-${kana10}=${Math.round(wakarinikusaIndex * 100) / 100})</span>`;
+                    resultText = resultText + detailText;
+                }
+     
+                const colorIndex = Math.round(256 * Math.max(0,(1 - wakarinikusaIndex)));
+                resultText = resultText + `<span style="background-color:rgb(255,${colorIndex},${colorIndex});">${sentence}</span>`;
             } else {
                 resultText = resultText + `<span>${sentence}</span>`;
             }
@@ -48,7 +48,7 @@ BTN_EXEC.onclick = function () {
     DIV_RESULT.innerHTML = resultText;
 };
 
-function countKanaOver9(str) {
+function countKana10(str) {
     var strs = str.match(/[ァ-ンー]{10,}/g);
     if (strs === null) {
         return 0;
@@ -57,7 +57,7 @@ function countKanaOver9(str) {
     }
 }
 
-function countKanjiOver5(str) {
+function countKanji6(str) {
     // var strs = str.match(/([\u3005\u3007\u303b\u3400-\u9FFF\uF900-\uFAFF\u20000-\u2FFFF][\uE0100-\uE01EF\uFE00-\uFE02]){2,}/g);
     // var strs = str.match(/([\u3005\u3007\u303b\u3400-\u49FFF\uF900-\uFAFF\u20000-\u2FFFF]{6,})/g);
     // var strs = str.match(/[\u3005\u3007\u303b\u4e00-\u9fcf\u3400-\u4dbf\uF900-\uFAdF\u20000-\u2a6df\u2f800-\u2fa1f]{6,}/g);
